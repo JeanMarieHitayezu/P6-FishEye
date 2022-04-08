159fb26b63a837cd7ff55231c7c5dd937a3cbfbc
Jean Marie Hitayezu <hitayezujeanmarie@gmail.com>
2022-04-08 11:53:36 +0200

    Home page code initiation

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,42 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+export default class Photographer {
 
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+    constructor(data) {
+        this.name = data.name;
+        this.id = data.id;
+        this.location = `${data.city}, ${data.country}`;
+        this.tagline = data.tagline;
+        this.price = data.price;
+        this.picture = `assets/photographers/photographers_ID_photos/${data.alt}`;
+        this.media = [data.media];
     }
-    return { name, picture, getUserCardDOM }
+    
+    getUserCardDOM() {
+        
+        return `
+        <article class="">
+            <a href="/photographer.html?id=${this.id}" aria-label="${this.name} - Fisheye">
+                <img class="user" src="./${this.picture}" alt="${this.name}" />
+
+                <h2 class="photographer__heading">
+                    ${this.name}
+                </h2>
+            </a>
+
+            <div class="photographer-datas">
+                <p class="photographer-datas_location" lang="en">
+                    ${this.location} 
+                </p>        
+
+                <p class="photographer-datas_tagline">
+                    ${this.tagline}
+                </p>
+
+                <p class="photographer-datas_price-per-day">
+                    ${this.price}€/jour
+                </p>
+            </div>
+        </article>`;
+
+    
+    }
 }
