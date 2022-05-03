96a0a7bc7a0cb314fa6ed24f9066e3c36b3f231e
Jean Marie Hitayezu <hitayezujeanmarie@gmail.com>
2022-05-03 09:42:11 +0200

    Completion of the modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,33 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
-}
+// DOM ELEMENTS FORM FIELDS VALIDATION
+let modal = document.getElementById('contact_modal');
+let firstName = document.getElementById('prenom');
+let lastName = document.getElementById('nom');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+let closeForm = document.querySelector('.contact-button');
 
+ 
+
+// LISTENER
+modal.addEventListener("submit", submitForm);
+closeForm.addEventListener("click", closeModal);
+
+// OPEN AND SEND FORM
+function displayModal() {
+    modal.style.display = "block";
+    modal.setAttribute("aria-hidden", "false");
+    modal.focus();
+}
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+} 
+
+function submitForm(e) {
+    e.preventDefault();
+    console.log('Prénom : ' + firstName.value);
+    console.log('Nom : ' + lastName.value);
+    console.log('Email : ' + email.value);
+    console.log('Message : ' + message.value);
+    closeModal();    
 }
